c35800059ccb27f58ee08e06009c214ec03fe095
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-11 21:24:39 +0530

    com

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const app = express();
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000.')
 })
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+});
+
+app.get('/welcome', (req, res)=>{
+    res.send("Welcome to the world of express");
+})
