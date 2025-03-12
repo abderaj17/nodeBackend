3087aaa901d04ec64b24264fe901873af6b2deb7
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-12 22:19:50 +0530

    com

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ app.get('/welcome', (req, res)=>{
     res.send("Welcome to the world of express");
 });
 
+
+const authMiddleware = (req, res, next)=>{
+
+}
+
+app.use((req, res, next) =>{
+    console.log("Middleware")
+    next();
+})
+
+app.get('/secret', (req, res)=>{
+res.send("Secret page")
+})
+
+
+
+const requestLogger = (req, res, next)=>{
+    const currentTime = new Date().toLocaleString();
+    console.log(`${currentTime} ${req.method} ${req.url}`);
+    res.send(currentTime, req.method, req.url);
+    next();
+}
+app.use(requestLogger);
