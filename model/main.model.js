49a4bf3b5eb68b5af09a6be1f26f8915deb9b2d5
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-17 20:52:14 +0530

    com

--- model/main.model.js
+++ model/main.model.js
@@ -12,5 +12,33 @@ const userSchema = new mongoose.Schema({
 
         trim: true,
         lowercase: true,
+    },
+
+    email:{
+        type: String,
+        lowercase: true,
+
+        validate:{
+            validator: function(value){
+                return /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(value);
+            },
+    age:{
+        type:Number,
+        required:[true, "Age is required"],
+        min:[18, "Age should be greater than 18"]
+    }
+    ,
+    role:{
+        type: String,
+        required: true,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    address:{
+        type:Number,
+        required: false,
+        trim: true,
+    }
+        }
     }
 })
