21fe42637a3a9cc40301876407286cd6375ffe9e
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-17 20:38:04 +0530

    com

--- model/main.model.js
+++ model/main.model.js
@@ -1,2 +1,16 @@
 //data modeling
 //data validation
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+
+        required: [true, `Email is required`],
+        unique: [true, `Email already exists`],
+
+        trim: true,
+        lowercase: true,
+    }
+})
