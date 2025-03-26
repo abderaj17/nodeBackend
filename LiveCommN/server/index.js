91d934c0ea75d98756f6902e7d8c0a0c46072de3
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-26 14:06:12 +0530

    om

--- LiveCommN/server/index.js
+++ LiveCommN/server/index.js
@@ -1,27 +1,36 @@
 const express = require('express')
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
+
 const http = require('http').createServer(app);
 
 //this creates a server that will be used to stablish to live connection for 
 //our socket.io server
 const socket = require('socket.io');
 
-
+ 
 
 const io = new socket.Server(http, {
     cors:{
         origin: '*',
         methods: ['GET', 'POST']
     }
-})
+});
 
 io.on('connection', (socket) => {   
     console.log(`a user connected with socket id ${socket.id}`);
 
-    socket.on('message', (msg)=>{
-        console.log(msg);
-        io.emit('message', msg);
+    socket.on('message', (data)=>{
+        console.log(data.message);
+        io.to(data.receiver).emit('fwd-message', data.message);
+    });
+
+    socket.on('join-room', (room) =>{
+      socket.join(room);
+      console.log(`User with socket id ${socket.id} joined room ${room}`);
     })
 });
 
